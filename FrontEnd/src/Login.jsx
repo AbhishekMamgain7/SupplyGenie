c1f87afea0479cf8dca296ad1dcd6387808b460d
Mahak-Sharma <146562226+Mahak-Sharma@users.noreply.github.com>
2025-03-09 20:31:02 +0530

    Update Login.jsx

--- FrontEnd/src/Login.jsx
+++ FrontEnd/src/Login.jsx
@@ -7,8 +7,9 @@ import { Reset } from './control/ui/Reset';
 import { Logo } from './control/ui/Logo';
 import { Title } from './control/ui/Title';
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import './index.css';
-
+import Header from './Customer/options.jsx';
 export * from 'react-login-page';
 export * from './control/ui/Username';
 export * from './control/ui/Password';
@@ -19,11 +20,24 @@ export * from './control/ui/Logo';
 
 const RenderLogin = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("Select");
+  const navigate = useNavigate();
   const { blocks = {}, data } = useStore();
   const { fields, buttons } = data || { fields: [] };
 
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  const handleSubmit = () =>{
+    if(selectedOption === "Customer"){
+      navigate("/Customer-options");
+    }
+  };
   return (
     <Render>
+      <Header/>
       <div className="login-page-1-wrapper">
         <article>
           <header>
@@ -39,22 +53,28 @@ const RenderLogin = () => {
                 </label>
               );
             })}
-            
-      <section className="dropdown-section">
-      <button 
-        className="dropdown-btn" 
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        Select
-      </button>
 
-      {}
-      <ul className={`dropdown-menu ${isOpen ? "show" : ""}`}>
-        <li className="dropdown-item">Customer</li>
-        <li className="dropdown-item">Manager</li>
-        <li className="dropdown-item">Admin</li>
-      </ul>
-    </section>
+          <section className="dropdown-section">
+            <button 
+              className="dropdown-btn" 
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {selectedOption}
+            </button>
+
+            <ul className={`dropdown-menu ${isOpen ? "show" : ""}`}>
+              <li className="dropdown-item" onClick={() => handleSelect("Customer")}>
+                Customer
+              </li>
+              <li className="dropdown-item" onClick={() => handleSelect("Manager")}>
+                Manager
+              </li>
+              <li className="dropdown-item" onClick={() => handleSelect("Admin")}>
+                Admin
+              </li>
+            </ul>
+          </section>
+
           <section className="login-page-1-wrapper">
             {buttons
               .sort((a, b) => a.index - b.index)
@@ -64,6 +84,7 @@ const RenderLogin = () => {
                 return cloneElement(child, {
                   ...child.props,
                   key: item.name + idx,
+                  onClick: handleSubmit,
                 });
               })}
           </section>
