2ae8faaefd943ab9b099da53959dc950419aad0d
Mahak-Sharma <146562226+Mahak-Sharma@users.noreply.github.com>
2025-03-31 11:47:43 +0530

    Delete FrontEnd/src/control/ui directory

--- FrontEnd/src/control/ui/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import { Block } from 'react-login-page';
-
-export const Footer = (props) => {
-  const { keyname = 'footer', name = 'footer', ...elmProps } = props;
-  return <Block {...elmProps} name={keyname || name} tagName="footer" />;
-};
-
-Footer.displayName = 'Login.Footer';
