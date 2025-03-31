94c3f72b4081fcf0df99edb456b1b9ff2074e4aa
Mahak-Sharma <146562226+Mahak-Sharma@users.noreply.github.com>
2025-03-31 11:48:52 +0530

    Add files via upload

---
+++ FrontEnd/src/Stocks/Stocks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import './Stocks.css';
+const Stocks = () =>{
+    return(
+        <div className="stocks-container">
+        </div>
+    );
+};
+export default Stocks;
