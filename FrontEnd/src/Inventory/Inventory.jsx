b2ad80797cf491332570c9c0fe230f234d76aef5
Mahak-Sharma <146562226+Mahak-Sharma@users.noreply.github.com>
2025-03-31 11:49:32 +0530

    Add files via upload

---
+++ FrontEnd/src/Inventory/Inventory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import './Inventory.css';
+const Inventory = () =>{
+    return(
+        <div className="inventory-container">
+        </div>
+    );
+};
+export default Inventory;
