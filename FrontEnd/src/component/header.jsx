0e86b14e807512886fcbf68efa02ed77614e8cbb
Mahak-Sharma <146562226+Mahak-Sharma@users.noreply.github.com>
2025-03-31 11:55:14 +0530

    Add files via upload

--- FrontEnd/src/component/header.jsx
+++ FrontEnd/src/component/header.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
   const navigate = useNavigate();
-
   return (
     <header className="header">
       <div className="header-title">SupplyGenie</div>
@@ -17,12 +14,9 @@ const Header = () => {
             <Link to="/Login" className="header-link">Login</Link>
           </li>
           <li className="header-menu-item">
-            <Link to="/Privacy" className="header-link">Policies</Link>
+            <Link to="/Policies" className="header-link">Policies</Link>
           </li>
           <li className="header-menu-item">
-            <div className="header-search">
-              <FontAwesomeIcon icon={faSearch} className="header-search-icon" />
-            </div>
           </li>
         </ul>
       </nav>
