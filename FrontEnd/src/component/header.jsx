0baaa2e4ebe68249ce79b3f839b29a7a93692969
Mahak-Sharma <146562226+Mahak-Sharma@users.noreply.github.com>
2025-04-05 00:44:40 +0530

    Add files via upload

--- FrontEnd/src/component/header.jsx
+++ FrontEnd/src/component/header.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
-  const navigate = useNavigate();
   return (
     <header className="header">
       <div className="header-title">SupplyGenie</div>
       <nav className="header-nav">
         <ul className="header-menu">
           <li className="header-menu-item">
-            <Link to="/Login" className="header-link">Login</Link>
+            <Link to="/Login" className="header-link">
+              Login
+            </Link>
           </li>
           <li className="header-menu-item">
-            <Link to="/Policies" className="header-link">Policies</Link>
+            <Link to="/Policies" className="header-link">
+              Policies
+            </Link>
           </li>
-          <li className="header-menu-item">
-          </li>
+          <li className="header-menu-item"></li>
         </ul>
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
